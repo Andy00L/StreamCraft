64aa93081e754ceee9fbdd00ce966574f82abe80
Andy <andy.luemba@protonmail.com>
2024-03-17 15:54:49 -0400

    added analitics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "./components/theme-provider";
 import "./globals.css";
-import prisma from "./lib/db";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,8 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <SpeedInsights />
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
